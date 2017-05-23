b734c7000838a183f6e827956088f7918dfad3ea
solomontang <solomoncztang@gmail.com>
2017-05-22 18:51:43 -0700

    Webpage now adds blinking object to document

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -15,6 +15,6 @@ makeBlinkyDancer.prototype.step = function() {
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   //debugger;
-  console.log(this.$node);
+  //console.log(this.$node);
   this.$node.toggle();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -1,23 +1,15 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-  // this.top = top;
-  // this.left = left;
+  this.top = top;
+  this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
-  // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   //debugger;
   this.step();
-
-  // now that we have defined the dancer object, 
-  // we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  
   this.setPosition(top, left);
 };
 
 makeDancer.prototype.step = function() {
-  // the basic dancer doesn't do anything interesting at all on each step,
-  // it just schedules the next step
   setTimeout(() => { makeDancer.bind(this.step()); }, makeDancer.bind(this.timeBetweenSteps));
 
 };
